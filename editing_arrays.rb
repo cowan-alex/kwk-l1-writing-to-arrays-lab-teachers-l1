fa9e654cc72047c426408bc73558a0da8a845afd
Alex Cowan <alexandria.cowan.17@tfacorps.org>
2018-07-05 19:22:40 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -5,10 +5,8 @@ rainbow_colors = [
 ]
 
 def change_rainbow_colors
-  rainbow_colors[0] = "red"
-  rainbow_colors[1] = "light_red"
-  rainbow_colors[2] = "light_yellow"
-  puts rainbow_colors 
+  rainbow_colors =["red", "light_red", "light_yellow"]
+  puts rainbow_colors
 end
 
 def add_colors
