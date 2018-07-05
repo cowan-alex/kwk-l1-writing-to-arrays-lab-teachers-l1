5ec9e89feaad206a3d5a0f55fc3d86d3b8dcc698
Alex Cowan <alexandria.cowan.17@tfacorps.org>
2018-07-05 19:23:30 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -3,11 +3,13 @@ rainbow_colors = [
   "default",
   "light_cyan"
 ]
- # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
+
 def change_rainbow_colors
   rainbow_colors =["red", "light_red", "light_yellow"]
 end
 
 def add_colors
- 
+  rainbow_colors << "green"
+  rainbow_colors << "blue"
+  return rainbow_colors
 end
