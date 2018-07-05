f048a62a296f713ec7f00826b7c79a9595dbae95
Alex Cowan <alexandria.cowan.17@tfacorps.org>
2018-07-05 19:23:58 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -9,6 +9,7 @@ def change_rainbow_colors
 end
 
 def add_colors
+  rainbow_colors = ["red", "light_red", "light_yellow"]
   rainbow_colors << "green"
   rainbow_colors << "blue"
   return rainbow_colors
